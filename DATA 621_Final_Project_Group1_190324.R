21353c174e39c7dce78087c458313949467fe0ad
ZacharyHerold <36413010+ZacharyHerold@users.noreply.github.com>
2019-03-24 03:51:14 +0800

    Add files via upload

---
+++ DATA 621_Final_Project_Group1_190324.R
@@ -0,0 +1,102 @@
+
+library(tidyverse)
+library(caret)
+
+#loading data from github (raw)
+red <- data.frame(read.csv("https://raw.githubusercontent.com/jjohn81/DATA621_Final_Project/master/winequality-red.csv", sep = ";"))
+white <- data.frame(read.csv("https://raw.githubusercontent.com/jjohn81/DATA621_Final_Project/master/winequality-white.csv", sep = ";"))
+
+#checking that column names are the same
+names(red)
+names(white)
+
+#quality of reds from 3 to 8 (mean = 5.636)
+summary(red)
+
+#quality of whites from 3 to 9 (mean = 5.878)
+summary(white)
+
+#setting a type category, then binding the two datasets
+red$type <- as.factor("R")
+white$type <- as.factor("W")
+wines <- rbind(red,white)
+
+# 6497 obs. of  13 variables
+str(wines)
+  
+
+# Data Visualizations (to fill in) - boxplots, etc. 
+
+# Scatterplot matrix
+
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits=digits)[1]
+  txt <- paste(prefix, txt, sep="")
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * r)
+}
+
+pairs(~quality + fixed.acidity + volatile.acidity + citric.acid + residual.sugar + 
+        chlorides + free.sulfur.dioxide + total.sulfur.dioxide + density +pH + 
+        sulphates + alcohol + type, data = wines,
+      lower.panel=panel.smooth, upper.panel=panel.cor, pch=20, main="Wines Scatterplot Matrix")
+
+# One notes the high correlation between free.sulfur.dioxide and total.sulfur.dioxide (0.72),
+# between type and total.sulfur.dioxide (0.70),
+# between density and alcohol (0.69),
+# between type and volatile.aciditiy (0.65)
+
+
+
+# Setting first regression
+# Using 10-fold Cross-Validation
+# http://www.sthda.com/english/articles/38-regression-model-validation/157-cross-validation-essentials-in-r/
+
+# Define training control
+set.seed(123) 
+train.control <- trainControl(method = "cv", number = 10)
+
+# Train the model
+model <- train(quality ~ ., data = wines, method = "lm",
+               trControl = train.control)
+
+# Summarize the results
+print(model)
+summary(model)
+# noting that typeW has a negative coefficient, slightly surprising given that the whites had the higher mean for quality
+
+# noting that citric.acid is not statistically significant
+
+model <- train(quality ~ . -citric.acid, data = wines, method = "lm",
+               trControl = train.control)
+print(model)
+summary(model)
+# Adjusted R-squared:  0.2953 
+
+
+## PCA
+cwines <- wines[,1:11]
+prwine <- prcomp(cwines)
+summary(prwine)
+round(prwine$rot[,1],2)
+
+## When scaled PC1 proportion of variance drops from 95.38% to 27.54%. Total.sulfur.dioxide is the primary determinant, with most variables having no impact
+prwinec <- prcomp(cwines, scale=TRUE)
+summary(prwinec)
+
+# Looking at first primary component, acidity, chlorides, density, pH, sulphates and alcohol have a negative effect.
+round(prwinec$rot[,1],2)
+# For the second primary component, only pH and alcohol have a negative effect, with the others switching signs.
+round(prwinec$rot[,2],2)
+# Looking at third primary component, pH becomes strongly positive, but alcohol remains negative. Citric acid becomes strongly negative. Sugar and sulfur dioxide (free and total) are positive for each of the top three.
+round(prwinec$rot[,3],2)
+
+#These results run counter to the regression model, as alcohol and pH both have positive coefficients. 
+
+#Eigenvectors for the PCA
+matplot(1:11, prwinec$rot[,1:3], type="l", xlab="Factors", ylab
+        ="", col=1)
